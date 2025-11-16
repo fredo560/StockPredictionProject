ce0d50dfbb51fcb0a3061ade7fb814de0c72ff69
fredo560 <alfredoh094@gmail.com>
2025-11-15 18:22:16 -0600

    Update predict_amzn_next_day.py

--- predict_amzn_next_day.py
+++ predict_amzn_next_day.py
@@ -18,7 +18,12 @@ end_date   = datetime.today().strftime('%Y-%m-%d')
 start_date = (datetime.today() - timedelta(days=70)).strftime('%Y-%m-%d')
 
 time.sleep(5)
-df = yf.download(ticker, start=start_date, end=end_date, progress=False, auto_adjust=True)
+try:
+    df = yf.download(ticker, start=start_date, end=end_date, progress=False, auto_adjust=True)
+except Exception as e:
+    time.sleep(15)
+    df = yf.download(ticker, start=start_date, end=end_date, progress=False, auto_adjust=True)
+
 print(df.head())
 
 # ================================
@@ -190,5 +195,6 @@ else:
     pred_df.to_csv(csv_file, mode='w', header=True, index=False)
 
     print(f"\nCreated and saved: {csv_file}")
+
 
 
