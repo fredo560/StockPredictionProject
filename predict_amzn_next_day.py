b0853cf946a7ffab2ef7fac6b06d490634141d2e
fredo560 <alfredoh094@gmail.com>
2025-11-15 18:07:08 -0600

    Refactor and clean up predict_amzn_next_day.py

--- predict_amzn_next_day.py
+++ predict_amzn_next_day.py
@@ -17,6 +17,7 @@ end_date   = datetime.today().strftime('%Y-%m-%d')
 start_date = (datetime.today() - timedelta(days=70)).strftime('%Y-%m-%d')
 
 df = yf.download(ticker, start=start_date, end=end_date, progress=False)
+print(df.head())
 
 # ================================
 # 2. REMOVE FIRST 3 ROWS & RENAME COLUMNS
@@ -185,4 +186,5 @@ if os.path.exists(csv_file):
 else:
     # Create new file with header
     pred_df.to_csv(csv_file, mode='w', header=True, index=False)
+
     print(f"\nCreated and saved: {csv_file}")
